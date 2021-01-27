bfd2a577d75f2843986a4037f6d1672745fd2e0f
Elida Arrechea <es222vs@student.lnu.se>
2021-01-27 21:09:37 +0100

    salt and hash password

--- src/models/crud-snippets.js
+++ src/models/crud-snippets.js
@@ -6,6 +6,7 @@
  */
 
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 
 // Create a schema.
 const schema = new mongoose.Schema({
@@ -22,3 +23,28 @@ const schema = new mongoose.Schema({
 
 // Create a model using the schema.
 export const Snippet = mongoose.model('Snippet', schema)
+
+// Create a schema fro login.
+const schemaLogin = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    minlength: [10, 'The password must be of minimum length 10 characters']
+  }
+}, {
+  timestamps: true,
+  versionKey: false
+})
+
+// Salts and hashes password before saving it to database.
+schemaLogin.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 8)
+})
+
+// Create a model using the schema.
+export const Login = mongoose.model('Login', schemaLogin)
